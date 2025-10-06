e86888616ddd951d3cf7ff809b1fe82747106825
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-10-06 16:42:42 +0300

    Add files via upload

---
+++ Prototype/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+    public class Customer:Person
+    {
+        public string City { get; set; }
+        public override Person Clone()
+        {
+            return (Person) MemberwiseClone();
+        }
+    }
+        
+    }
